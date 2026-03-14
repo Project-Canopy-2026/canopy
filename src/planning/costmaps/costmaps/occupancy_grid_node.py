52266c4edaec1ecf6f7e08d1794c8e68f84b44bc
alina <alinaw@andrew.cmu.edu>
2026-03-14 14:39:06 -0400

    structure fix

--- src/planning/costmaps/costmaps/occupancy_grid_node.py
+++ src/planning/costmaps/costmaps/occupancy_grid_node.py
@@ -117,7 +117,7 @@ class OccupancyGridNode(Node):
 
         self.setUpParameters()
 
-        self.create_subscription(PointCloud2, "/velodyne_points", self.pcdCb, 1)
+        self.create_subscription(PointCloud2, "/vlp16/depth_pcd", self.pcdCb, 1)
 
         self.occ_grid_pub = self.create_publisher(OccupancyGrid, "/cost/occupancy", 1)
 
