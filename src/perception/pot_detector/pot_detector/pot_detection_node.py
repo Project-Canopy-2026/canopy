14ab15dc816363415133f0bd93a45f7fe3ead692
Neha Kayiti <nkayiti@gmail.com>
2026-03-31 19:50:58 -0400

    modified initial setup and got intrinsics, not tested yet

--- src/perception/pot_detector/pot_detector/pot_detection_node.py
+++ src/perception/pot_detector/pot_detector/pot_detection_node.py
@@ -9,6 +9,7 @@ from ultralytics import YOLO
 import message_filters
 import cv2
 import numpy as np
+from std_msgs.msg import Bool
 
 class SeedlingDetector(Node):
     def __init__(self):
@@ -19,26 +20,54 @@ class SeedlingDetector(Node):
         self.grasp_pub = self.create_publisher(PoseStamped, '/seedling/grasp_point', 10)
         self.vis_pub = self.create_publisher(Image, '/seedling/annotated_image', 10)
 
-        # ROS2 subscribers — synchronized RGB + depth
+        # ROS2 subscribers
+        depth_camera_info = self.create_subscription(sensor_msgs/CameraInfo, '/camera/camera/aligned_depth_to_color/camera_info', self.depth_intrinsics_callback, 10)        
+        pre_grasp_ready = self.create_subscription(Bool, '/behavior/pre_grasp_ready', self.pot_detection_callback, 10)
+
         rgb_sub = message_filters.Subscriber(self, Image, '/camera/color/image_raw')
-        depth_sub = message_filters.Subscriber(self, Image, '/camera/depth/image_raw')
+        depth_sub = message_filters.Subscriber(self, Image, '/camera/camera/aligned_depth_to_color/image_raw')
         self.ts = message_filters.ApproximateTimeSynchronizer(
             [rgb_sub, depth_sub], queue_size=10, slop=0.05
         )
         self.ts.registerCallback(self.image_callback)
         
-
         self.bridge = CvBridge()
         
         self.model = YOLO('./src/perception/pot_detector/model/yolo11n/best.pt') # TODO: Change this to the correct path on Jetson
 
-    def image_callback(self, msg: Image, depth_msg: Image):
+        # Camera intrinsic parameters
+        self.fx = None
+        self.fy = None
+        self.cx = None
+        self.cy = None
+
+        self.latest_rgb_msg = None
+        self.latest_depth_msg = None
+
+
+    def image_callback(self, rgb_msg: Image, depth_msg: Image):
+        self.latest_rgb_msg = rgb_msg
+        self.latest_depth_msg = depth_msg
+
+
+    def pot_detection_callback(self, msg: Bool):
+        if not msg.Data:
+            return
+
+        if self.latest_rgb_msg is None or self.latest_depth_msg is None:
+            self.get_logger().warn("No synced RGB/depth image available yet")
+            return
+
         # Convert ROS images to OpenCV
-        cv_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-        depth_image = self.bridge.imgmsg_to_cv2(depth_msg, desired_encoding='passthrough')  # float32, metres
+        rgb_image = self.bridge.imgmsg_to_cv2(self.latest_rgb_msg, desired_encoding='bgr8')
+        
+        depth_image = self.bridge.imgmsg_to_cv2(self.latest_depth_msg, desired_encoding='passthrough')
+        # depth is encoded 26UC1
+        depth_imgage = np.asarray(depth_img, dtype=np.float32)
+        depth_image /= 1000
 
         # YOLO detection
-        results = self.model(cv_image)
+        results = self.model(rgb_image)
         boxes = results[0].boxes  # ultralytics Results API
 
         if boxes is None or len(boxes) == 0:
@@ -52,17 +81,22 @@ class SeedlingDetector(Node):
         cls = int(boxes.cls[best_idx].cpu())
 
         # Publish bounding box (as Float32MultiArray: [x1, y1, x2, y2, confidence])
-        bbox_msg = Float32MultiArray()
+        bbox_msg = Float32MultiArray()To prepare for your pre-employment screening, please research your own history and gather your personal information. This may include previous addresses, names of employers, employment dates, and job titles. This will allow you to provide complete and accurate information to ESS.
         bbox_msg.data = [x1, y1, x2, y2, conf]
         self.bbox_pub.publish(bbox_msg)
 
         # Compute pot center (for simplicity, take bbox center)
-        cx = (x1 + x2) / 2.0
-        cy = (y1 + y2) / 2.0
+        center_x = (x1 + x2) / 2.0
+        center_y = (y1 + y2) / 2.0
         self.get_logger().info(f'Detected pot. Pot center at pixel ({cx}, {cy}) with confidence {conf:.2f}')
         
-        # Look up depth at the pot center pixel
-        depth = float(depth_image[int(cy), int(cx)])
+        # Get depth at the pot center pixel
+        depth_center = float(depth_image[int(center_y), int(center_x)])
+
+        # 3D projection
+        # will move to separate function later to clean up
+
+
 
         # Convert image coordinates to world coordinates (placeholder)
         # grasp_pose = cal_grasp_pose(cx, cy, depth)
@@ -75,6 +109,16 @@ class SeedlingDetector(Node):
         cv2.circle(vis_image, (int(cx), int(cy)), 4, (0, 0, 255), -1)
         self.vis_pub.publish(self.bridge.cv2_to_imgmsg(vis_image, encoding='bgr8'))
 
+
+    def depth_intrinsics_callback(self, msg):
+        # K: [fx, 0, cx, 0, fy, cy, 0, 0, 1]
+
+        self.fx = msg.K[0]
+        self.fy = msg.K[4]
+        self.cx = msg.K[2]
+        self.cy = msg.K[5]
+
+    
     def cal_grasp_pose(self, cx, cy):
         grasp_pose = PoseStamped()
         # Placeholder for actual grasp pose calculation lca szogic
